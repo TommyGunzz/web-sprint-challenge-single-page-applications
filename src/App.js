8199c33dd5850ead61ab84a96498209cb5047014
Tom Harris <tom_3rd_2000@yahoo.com>
2020-09-13 09:13:25 -0400

    revised import exports, src/App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import NewForm from "./components/Form";
-import { Container, Row, Col } from "reactstrap";
-import axios from "axios";
-import UserTable from "./components/UserTable";
+import { Route, Switch } from "react-router-dom";
+import PizzaForm from "./components/PizzaForm";
+import PizzaHome from "./components/PizzaHome";
+import Tracker from "./components/Tracker";
 
 const App = () => {
   return (
